d954f2e231c7fbc0d487a24b9d87628148b42521
Frame1910 <dpmeiring@gmail.com>
2017-08-31 21:22:52 +0800

    Stuff

--- wage.js
+++ wage.js
@@ -1,4 +1,3 @@
-var employeePay = 0;
 var payRate = 0;
 function manager() {
     if (document.getElementById("position").checked === true) {
@@ -9,9 +8,25 @@ function manager() {
         console.log(payRate);
     }
 }
+
+
+
+var normalPay = 0;
+var normalHours = 0;
+function normalSalary() {
+    var normalHours = document.getElementById("normalHoursTotal").value;
+    var normalPay = normalHours * payRate;
+    document.getElementById("normalPay").innerHTML = "$" + normalPay;
+}
+
+
+
 var checkedValue = 0;
 function daysWorked(dayNumber) {
-    if (document.getElementById(dayNumber.toString()).checked == true) {
-        checkedValue = checkedValue + 1
+    if (document.getElementById(dayNumber.toString()).checked === true) {
+        checkedValue = checkedValue + 1;
+    } else {
+        checkedValue = checkedValue - 1;
     }
+    document.getElementById('totalDaysWorked').innerHTML = checkedValue.toString();
 }
