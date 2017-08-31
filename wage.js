710bb2160b454cb07f7cbda90c56dfc13d6c5e80
Darren Meiring <Frame1910@users.noreply.github.com>
2017-08-31 13:35:20 +0800

    Completed Layout started JS

---
+++ wage.js
@@ -0,0 +1,14 @@
+var employeePay = 0;
+var payRate = 0;
+function manager() {
+    if (document.getElementById("position").checked === true) {
+        var payRate = 30;
+        console.log(payRate);
+    } else {
+        var payRate = 23;
+        console.log(payRate);
+    }
+}
+function daysWorked() {
+    
+}
