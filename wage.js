e5aff9e27e3ef27b9a00bdbced9e11a5ec48fa37
Frame1910 <Frame1910@users.noreply.github.com>
2017-09-01 10:00:02 +0800

    JS progress

--- wage.js
+++ wage.js
@@ -1,25 +1,20 @@
-var payRate = 0;
+var payRate = 23;
 function manager() {
     if (document.getElementById("position").checked === true) {
-        var payRate = 30;
-        console.log(payRate);
+        window.payRate = 30;
     } else {
-        var payRate = 23;
-        console.log(payRate);
+        window.payRate = 23;
     }
 }
-
-
 
 var normalPay = 0;
 var normalHours = 0;
 function normalSalary() {
+    console.log(payRate);
     var normalHours = document.getElementById("normalHoursTotal").value;
     var normalPay = normalHours * payRate;
     document.getElementById("normalPay").innerHTML = "$" + normalPay;
 }
-
-
 
 var checkedValue = 0;
 function daysWorked(dayNumber) {
@@ -29,4 +24,44 @@ function daysWorked(dayNumber) {
         checkedValue = checkedValue - 1;
     }
     document.getElementById('totalDaysWorked').innerHTML = checkedValue.toString();
+}
+
+var weekDayOvertimeHours = 0;
+var weekDayOvertimeRate = 1.25;
+var weekDayOvertimePay = 0;
+
+function weekDayOvertimeSalary() {
+    console.log(payRate);
+    var weekDayOvertimeHours = document.getElementById("weekDayOvertime").value;
+    var weekDayOvertimePay = weekDayOvertimeHours * payRate * weekDayOvertimeRate;
+    document.getElementById("weekDayOvertimePay").innerHTML = "$" + weekDayOvertimePay;
+}
+
+var weekDayOvertimeHours2 = 0;
+var weekDayOvertimeRate2 = 1.45;
+var weekDayOvertimePay2 = 0
+
+function weekDayOvertimeSalary2() {
+    console.log(payRate);
+    var weekDayOvertimeHours2 = document.getElementById("weekDayOvertime2").value;
+    var weekDayOvertimePay2 = weekDayOvertimeHours2 * payRate * weekDayOvertimeRate2;
+    document.getElementById("weekDayOvertimePay2").innerHTML = "$" + weekDayOvertimePay2;
+}
+
+var satHours = 0;
+var satPay = 0;
+
+function satSalary() {
+    console.log(payRate);
+    var satHours = document.getElementById("saturdayNormal").value;
+    var satpay = satHours * payRate + 3;
+    document.getElementById("satPay").innerHTML = "$" + satpay;
+}
+
+var sunHolNormalHours = 0;
+var sunHolPay = 0;
+
+function sunHolSalary() {
+    console.log(payRate);
+    var sunHolHours = 
 }
